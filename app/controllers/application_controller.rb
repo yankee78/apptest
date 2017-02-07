b4ebb56fd808ae777838f075f46658aadad4ec7b
Njara <njararazafi@gmail.com>
2017-02-07 22:46:51 +0100

    new pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def not_found
+  	render :file => "#{Rails.root}/public/404.html", :status => 404
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	resources :homes
+
+	get '*unmatched_route', to: 'application#not_found'
+	
+	root to: "homes#static_index"
 end
