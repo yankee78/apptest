d76cad19cf5b58931435a3ab4f520dfd31148ba9
Njara <njararazafi@gmail.com>
2017-02-11 18:21:34 +0100

    fixjs

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,36 +1,35 @@
 $(function(){
-	
-	var note = $('#note'),
-		ts = new Date(2012, 0, 1),
-		newYear = true;
-	
-	if((new Date()) > ts){
-		// The new year is here! Count towards something else.
-		// Notice the *1000 at the end - time must be in milliseconds
-		ts = (new Date()).getTime() + 10*24*60*60*1000;
-		newYear = false;
-	}
+	$(document).on('turbolinks:load', function(){
+		var note = $('#note'),
+			ts = new Date(2012, 0, 1),
+			newYear = true;
 		
-	$('#countdown').countdown({
-		timestamp	: ts,
-		callback	: function(days, hours, minutes, seconds){
-			
-			var message = "";
-			
-			message += days + " day" + ( days==1 ? '':'s' ) + ", ";
-			message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
-			message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
-			message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
-			
-			if(newYear){
-				message += "left until the new year!";
-			}
-			else {
-				message += "left to 10 days from now!";
-			}
-			
-			note.html(message);
+		if((new Date()) > ts){
+			// The new year is here! Count towards something else.
+			// Notice the *1000 at the end - time must be in milliseconds
+			ts = (new Date()).getTime() + 10*24*60*60*1000;
+			newYear = false;
 		}
+		$('#countdown').countdown({
+			timestamp	: ts,
+			callback	: function(days, hours, minutes, seconds){
+				
+				var message = "";
+				
+				message += days + " day" + ( days==1 ? '':'s' ) + ", ";
+				message += hours + " hour" + ( hours==1 ? '':'s' ) + ", ";
+				message += minutes + " minute" + ( minutes==1 ? '':'s' ) + " and ";
+				message += seconds + " second" + ( seconds==1 ? '':'s' ) + " <br />";
+				
+				if(newYear){
+					message += "left until the new year!";
+				}
+				else {
+					message += "left to 10 days from now!";
+				}
+				
+				note.html(message);
+			}
+		});
 	});
-	
 });
